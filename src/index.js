59922bb160189f3a1207b85436a4e01296704630
Kushank Sriraj <kushanksriraj@gmail.com>
2020-12-08 19:39:15 +0530

    fix: prime logic

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const primeCheckButton = document.getElementById("primeCheckButton");
 const primeCheck = document.getElementById("primeCheck");
 
 function checkPrimeNumber(n) {
-  for (var i = 2; i < Math.sqrt(n); i++) {
+  for (var i = 2; i <= Math.sqrt(n); i++) {
     if (n % i == 0) {
       return false;
     }
